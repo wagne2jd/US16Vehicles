24af2e293cdbc28aef923312652c4b2ea6686dfa
windows7 <windows7@windows7-PC>
2016-05-26 18:02:21 -0400

    add gallonsOfGas and mgp + getters and setters

--- src/vehicles/Vehicle.java
+++ src/vehicles/Vehicle.java
@@ -11,4 +11,34 @@ package vehicles;
  */
 public class Vehicle {
     
+    private double gallonsOfGas;
+    private int mpg;
+
+    /**
+     * @return the gallonsOfGas
+     */
+    public double getGallonsOfGas() {
+        return gallonsOfGas;
+    }
+
+    /**
+     * @param gallonsOfGas the gallonsOfGas to set
+     */
+    public void setGallonsOfGas(double gallonsOfGas) {
+        this.gallonsOfGas = gallonsOfGas;
+    }
+
+    /**
+     * @return the mpg
+     */
+    public int getMpg() {
+        return mpg;
+    }
+
+    /**
+     * @param mpg the mpg to set
+     */
+    public void setMpg(int mpg) {
+        this.mpg = mpg;
+    }
 }
